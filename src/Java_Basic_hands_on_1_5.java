f5596d74f75f692a7163155a9503a94bd194d898
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-08 10:19:08 +0700

    latihan 1.5

---
+++ src/Java_Basic_hands_on_1_5.java
@@ -0,0 +1,16 @@
+
+public class Java_Basic_hands_on_1_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		insiaasi varibel i dengan tipe data integer
+		int i = 1;
+//		lakukan perulangan jika kondisi (while) terpenuhi
+	      do {
+	         System.out.println("no. " + i);
+	         i++;
+	      } while (i<=6);
+
+	}
+
+}
