914ec994daba3bbf7a62170aefada646a1b6cc87
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-08 10:14:32 +0700

    latihan 1.4

---
+++ src/Java_Basic_hands_on_1_4.java
@@ -0,0 +1,18 @@
+
+public class Java_Basic_hands_on_1_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		inisiasi variabel i dengan tipe data integer
+		int i = 0;
+//		kondisi dilakukan nya perulangan jika kondisi tersebut true
+	      while (i<=6) {
+//	    	  statemen yang dilakukan jika kondisi tersebut terpenuhi
+	         System.out.println("no. " + i);
+//	         perulangan i
+	         i++;
+	      }
+
+	}
+
+}
